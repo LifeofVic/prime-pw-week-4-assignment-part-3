3f86ce3960212feac11cca05763334de176167eb
Victor Llapa <victorllapa@gmail.com>
2022-10-15 21:43:10 -0500

    Created a function to add items to basket array as well a s made sure if no items were inclued in the parameters then it will return false.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,7 +10,18 @@ function addItem(item) {
 		basket.push(item)
 		return true
 	}
-	else false;
+	else
+		return false;
 }
 
+console.log('Adding random items to basket array. ', addItem('Apple'));
+console.log('Adding random items to basket array. ', addItem('Cereal'));
+console.log('Adding random items to basket array. ', addItem('Chips'));
+console.log('Adding random items to basket array. ', addItem('Milk'));
+console.log('Adding random items to basket array. ', addItem('Mangoes'));
+
+console.log('Adding zero items to basket array. [false] ', addItem());
+
+console.log('Checking the current items on "basket", ', basket);
+console.log('Function of adding item to basket Array is working properly as well as adding zero items in the parameters.');
 
