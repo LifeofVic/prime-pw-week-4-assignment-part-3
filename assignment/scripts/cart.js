5fd4145e7a3daafe3060e2d44018de5112bdecdc
Victor Llapa <victorllapa@gmail.com>
2022-10-15 21:04:11 -0500

    Created a function to add 'item to the basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,16 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+let basket = [];
+
+function addItem(item) {
+	if (item) {
+		basket.push(item)
+		return true
+	}
+	else false;
+}
+
+
